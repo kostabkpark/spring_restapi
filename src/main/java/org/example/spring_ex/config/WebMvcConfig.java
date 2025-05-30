9db7218788dbd1b47f0c3126aaab8692d27129e9
kostabkpark <kostabkpark@gmail.com>
2025-05-30 10:34:31 +0900

    interceptor prehandle

--- src/main/java/org/example/spring_ex/config/WebMvcConfig.java
+++ src/main/java/org/example/spring_ex/config/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package org.example.spring_ex.config;
 
 import jakarta.servlet.Filter;
+import org.example.spring_ex.interceptor.LogInterceptor;
+import org.example.spring_ex.interceptor.LogInterceptor2;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +16,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Bean
   Filter shallowEtagHeaderFilter() {
     return new ShallowEtagHeaderFilter();
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+        .addPathPatterns("/**")
+        .order(1);
+
+    registry.addInterceptor(new LogInterceptor2())
+        .addPathPatterns("/**")
+        .order(2);
   }
 }
