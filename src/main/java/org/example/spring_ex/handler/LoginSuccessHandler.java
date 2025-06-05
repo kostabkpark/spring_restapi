a46eeb9fdc356bea45dcd7616ce53ecffcee45f7
kostabkpark <kostabkpark@gmail.com>
2025-06-05 11:39:36 +0900

    security login

--- src/main/java/org/example/spring_ex/handler/LoginSuccessHandler.java
+++ src/main/java/org/example/spring_ex/handler/LoginSuccessHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.example.spring_ex.model.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -19,7 +21,10 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     User user = (User)authentication.getPrincipal();
     session.setAttribute("username", authentication.getName());
     session.setAttribute("userid", user.getUserid());
-    // ???
+
+    log.info("Username : {} logged in ", session.getAttribute("username").toString());
+    log.info("User - userid {} ",session.getAttribute("userid").toString());
+
     response.sendRedirect("/posts");
   }
 }
